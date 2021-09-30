1475069eac638c403573dd1cd5d8558eeb7e5a7c
Mantas Pilipuitis <mantas.pilipuitis@gmail.com>
2021-09-30 17:40:43 +0300

    Split interfaces

---
+++ app/src/main/java/Interfaces/TextViewInput.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+import android.view.View;
+
+public interface TextViewInput extends WorkoutInput {
+    void setText(CharSequence text);
+}
--- app/src/main/java/Interfaces/WorkoutInput.java
+++ app/src/main/java/Interfaces/WorkoutInput.java
@@ -3,8 +3,7 @@ package Interfaces;
 import android.view.View;
 
 public interface WorkoutInput {
-    void setText(CharSequence text);
+    void setTextClickListener();
     void setOnLongClickListener(View.OnLongClickListener onLongClickListener);
     void setOnClickListener(View.OnClickListener onClickListener);
-    void setTextEditListener();
 }
