301a8697dec121f936d30d8a504003daaf6b0609
Mantas Pilipuitis <mantas.pilipuitis@gmail.com>
2021-10-18 14:46:43 +0300

    Fix comment edit bug

--- app/src/main/java/DataEdit/ImageViews/WorkoutCommentView.java
+++ app/src/main/java/DataEdit/ImageViews/WorkoutCommentView.java
@@ -6,12 +6,16 @@ import android.util.AttributeSet;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.workoutbasic.R;
 
 import DataEdit.DataEditFragments.Text.CommentEditFragment;
+import DataEdit.DataEditFragments.TextFragments;
 import Interfaces.TextViewData;
+import Utils.FragmentMethods;
 import ViewModels.SharedViewModel;
 
 public class WorkoutCommentView extends WorkoutImageView {
@@ -22,6 +26,15 @@ public class WorkoutCommentView extends WorkoutImageView {
 
     @Override
     public void setTextClickListener() {
-        setOnClickListener(view -> navController.navigate(R.id.action_editExerciseFragment_to_commentEditFragment));
+        setOnClickListener(view -> {
+            navController.navigate(R.id.action_editExerciseFragment_to_commentEditFragment);
+            FragmentManager fm = ((FragmentActivity)getContext()).getSupportFragmentManager();
+            NavHostFragment navHostFragment = (NavHostFragment)fm.findFragmentById(R.id.nav_host_fragment);
+            navHostFragment.getChildFragmentManager().executePendingTransactions();
+
+            TextFragments calledFragment = (TextFragments) FragmentMethods.getParentFragment(getContext(), 1);
+            SharedViewModel viewModel = new ViewModelProvider(calledFragment).get(SharedViewModel.class);
+            viewModel.select((TextViewData) parentData);
+        });
     }
 }
